673fe79a07810779fccc85442da7aff589d387c5
Siddharth <mail@siddharthjha.com>
2019-12-30 01:25:37 +0530

    added new fonts

--- js/app.js
+++ js/app.js
@@ -1,12 +1,1 @@
-// const swup = new Swup();
-
-const navTail = document.querySelector('.nav-tail');
-const links = document.querySelectorAll(".nav-links li");
-const burger = document.querySelector('.burger');
-
-burger.addEventListener('click', () => {
-    navTail.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fade");
-    });
-});
+console.log("Hello MK!");
