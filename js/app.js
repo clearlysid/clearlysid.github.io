b592fffdfc040c1a7efc2e758cf3e284b5431724
Siddharth <mail@siddharthjha.com>
2019-12-30 19:12:10 +0530

    cleaned up homescreen, added sm icons

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 console.log("Hello MK!");
 
-var name = prompt("What's your name?", "Mehak");
-document.getElementById("msg").innerHTML = "Hello " + name + "!";
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 5000
+  });
