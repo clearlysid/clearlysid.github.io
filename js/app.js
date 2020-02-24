4af628de59439415c70d85668c6305b2008c907a
Siddharth <mail@siddharthjha.com>
2020-02-24 20:35:40 +0530

    gave up on page transitions

--- js/app.js
+++ js/app.js
@@ -110,191 +110,190 @@ primaryNavContainer.addEventListener("mouseleave", function( event ) {
 
 
 function onContentReplaced() {
+    // Tilt Effect for Projects
+    {
+        const lineEq = (y2, y1, x2, x1, currentVal) => {
+            // y = mx + b 
+            var m = (y2 - y1) / (x2 - x1), b = y1 - m * x1;
+            return m * currentVal + b;
+        };
 
+        const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+        const getRandomFloat = (min, max) => (Math.random() * (max - min) + min).toFixed(2);
 
-// Tilt Effect for Projects
-
-{
-    const lineEq = (y2, y1, x2, x1, currentVal) => {
-        // y = mx + b 
-        var m = (y2 - y1) / (x2 - x1), b = y1 - m * x1;
-        return m * currentVal + b;
-    };
-
-    const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-    const getRandomFloat = (min, max) => (Math.random() * (max - min) + min).toFixed(2);
-
-    const setRange = (obj) => {
-        for(let k in obj) {
-            if( obj[k] == undefined ) {
-                obj[k] = [0,0];
-            }
-            else if( typeof obj[k] === 'number' ) {
-                obj[k] = [-1*obj[k],obj[k]];
-            }
-        }
-    };
-
-    const getMousePos = (e) => {
-        let posx = 0;
-        let posy = 0;
-		if (!e) e = window.event;
-		if (e.pageX || e.pageY) 	{
-			posx = e.pageX;
-			posy = e.pageY;
-		}
-		else if (e.clientX || e.clientY) 	{
-			posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-			posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-		}
-		return { x : posx, y : posy }
-	};
-    
-    class Item {
-		constructor(el, options) {
-            this.DOM = {el: el};
-            this.options = {   
-                image: {
-                    translation : {x: -10, y: -10, z: 0},
-                    rotation : {x: 0, y: 0, z: 0},
-                    reverseAnimation : {
-                        duration : 1200,
-                        easing : 'easeOutElastic',
-                        elasticity : 600
-                    }
-                },
-                title: {
-                    translation : {x: 20, y: 10, z: 0},
-                    reverseAnimation : {
-                        duration : 1000,
-                        easing : 'easeOutExpo',
-                        elasticity : 600
-                    }
-                },
-                shadow: {
-                    translation : {x: 30, y: 20, z: 0},
-                    rotation : {x: 0, y: 0, z: -2},
-                    reverseAnimation : {
-                        duration: 2,
-                        easing: 'easeOutElastic'
-                    }
-                }, 
-            };
-
-            Object.assign(this.options, options);
-            
-            this.DOM.animatable = {};
-            this.DOM.animatable.image = this.DOM.el.querySelector('.project-item-img');
-            this.DOM.animatable.title = this.DOM.el.querySelector('.project-item-title');
-            this.DOM.animatable.shadow = this.DOM.el.querySelector('.project-item-bg');
-            
-            this.initEvents();
-        }
-        
-        initEvents() { 
-            let enter = false;
-            this.mouseenterFn = () => {
-                if ( enter ) {
-                    enter = false;
-                };
-                clearTimeout(this.mousetime);
-                this.mousetime = setTimeout(() => enter = true, 40);
-            };
-            this.mousemoveFn = ev => requestAnimationFrame(() => {
-                if ( !enter ) return;
-                this.tilt(ev);
-            });
-            this.mouseleaveFn = (ev) => requestAnimationFrame(() => {
-                if ( !enter || !allowTilt ) return;
-                enter = false;
-                clearTimeout(this.mousetime);
-
-                for (let key in this.DOM.animatable ) {
-                    if( this.DOM.animatable[key] == undefined || this.options[key] == undefined ) {continue;}
-                    anime({
-						targets: this.DOM.animatable[key],
-						duration: this.options[key].reverseAnimation != undefined ? this.options[key].reverseAnimation.duration || 0 : 1.5,
-						easing: this.options.reverseAnimation != undefined ? this.options.movement[key].reverseAnimation.easing || 'linear' : 'linear',
-						elasticity: this.options.reverseAnimation != undefined ? this.options.reverseAnimation.elasticity || null : null,
-						scaleX: 1,
-						scaleY: 1,
-						scaleZ: 1,
-						translateX: 0,
-						translateY: 0,
-						translateZ: 0,
-						rotateX: 0,
-						rotateY: 0,
-						rotateZ: 0
-					});                    
+        const setRange = (obj) => {
+            for(let k in obj) {
+                if( obj[k] == undefined ) {
+                    obj[k] = [0,0];
                 }
-            });
-            this.DOM.el.addEventListener('mouseenter', this.mouseenterFn);
-            this.DOM.el.addEventListener('mousemove', this.mousemoveFn);
-            this.DOM.el.addEventListener('mouseleave', this.mouseleaveFn);
-        }
-        
-        tilt(ev) {
-            if ( !allowTilt ) return;
-            const mousepos = getMousePos(ev);
-            // Document scrolls.
-            const docScrolls = {
-                left : document.body.scrollLeft + document.documentElement.scrollLeft, 
-                top : document.body.scrollTop + document.documentElement.scrollTop
-            };
-            const bounds = this.DOM.el.getBoundingClientRect();
-            // Mouse position relative to the main element (this.DOM.el).
-            const relmousepos = { 
-                x : mousepos.x - bounds.left - docScrolls.left, 
-                y : mousepos.y - bounds.top - docScrolls.top 
-            };
-            
-            // Movement settings for the animatable elements.
-            for (let key in this.DOM.animatable) {
-                if ( this.DOM.animatable[key] == undefined || this.options[key] == undefined ) {
-                    continue;
+                else if( typeof obj[k] === 'number' ) {
+                    obj[k] = [-1*obj[k],obj[k]];
                 }
-                
-                let t = this.options[key] != undefined ? this.options[key].translation || {x:0,y:0,z:0} : {x:0,y:0,z:0},
-                    r = this.options[key] != undefined ? this.options[key].rotation || {x:0,y:0,z:0} : {x:0,y:0,z:0};
+            }
+        };
 
-                setRange(t);
-                setRange(r);
-                
-                const transforms = {
-                    translation : {
-                        x: (t.x[1]-t.x[0])/bounds.width*relmousepos.x + t.x[0],
-                        y: (t.y[1]-t.y[0])/bounds.height*relmousepos.y + t.y[0],
-                        z: (t.z[1]-t.z[0])/bounds.height*relmousepos.y + t.z[0],
+        const getMousePos = (e) => {
+            let posx = 0;
+            let posy = 0;
+            if (!e) e = window.event;
+            if (e.pageX || e.pageY) 	{
+                posx = e.pageX;
+                posy = e.pageY;
+            }
+            else if (e.clientX || e.clientY) 	{
+                posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+                posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+            }
+            return { x : posx, y : posy }
+        };
+        
+        class Item {
+            constructor(el, options) {
+                this.DOM = {el: el};
+                this.options = {   
+                    image: {
+                        translation : {x: -10, y: -10, z: 0},
+                        rotation : {x: 0, y: 0, z: 0},
+                        reverseAnimation : {
+                            duration : 1200,
+                            easing : 'easeOutElastic',
+                            elasticity : 600
+                        }
                     },
-                    rotation : {
-                        x: (r.x[1]-r.x[0])/bounds.height*relmousepos.y + r.x[0],
-                        y: (r.y[1]-r.y[0])/bounds.width*relmousepos.x + r.y[0],
-                        z: (r.z[1]-r.z[0])/bounds.width*relmousepos.x + r.z[0]
-                    }
+                    title: {
+                        translation : {x: 20, y: 10, z: 0},
+                        reverseAnimation : {
+                            duration : 1000,
+                            easing : 'easeOutExpo',
+                            elasticity : 600
+                        }
+                    },
+                    shadow: {
+                        translation : {x: 30, y: 20, z: 0},
+                        rotation : {x: 0, y: 0, z: -2},
+                        reverseAnimation : {
+                            duration: 2,
+                            easing: 'easeOutElastic'
+                        }
+                    }, 
                 };
 
-                this.DOM.animatable[key].style.WebkitTransform = this.DOM.animatable[key].style.transform = 'translateX(' + transforms.translation.x + 'px) translateY(' + transforms.translation.y + 'px) translateZ(' + transforms.translation.z + 'px) rotateX(' + transforms.rotation.x + 'deg) rotateY(' + transforms.rotation.y + 'deg) rotateZ(' + transforms.rotation.z + 'deg)';
+                Object.assign(this.options, options);
+                
+                this.DOM.animatable = {};
+                this.DOM.animatable.image = this.DOM.el.querySelector('.project-item-img');
+                this.DOM.animatable.title = this.DOM.el.querySelector('.project-item-title');
+                this.DOM.animatable.shadow = this.DOM.el.querySelector('.project-item-bg');
+                
+                this.initEvents();
+            }
+            
+            initEvents() { 
+                let enter = false;
+                this.mouseenterFn = () => {
+                    if ( enter ) {
+                        enter = false;
+                    };
+                    clearTimeout(this.mousetime);
+                    this.mousetime = setTimeout(() => enter = true, 40);
+                };
+                this.mousemoveFn = ev => requestAnimationFrame(() => {
+                    if ( !enter ) return;
+                    this.tilt(ev);
+                });
+                this.mouseleaveFn = (ev) => requestAnimationFrame(() => {
+                    if ( !enter || !allowTilt ) return;
+                    enter = false;
+                    clearTimeout(this.mousetime);
+
+                    for (let key in this.DOM.animatable ) {
+                        if( this.DOM.animatable[key] == undefined || this.options[key] == undefined ) {continue;}
+                        anime({
+                            targets: this.DOM.animatable[key],
+                            duration: this.options[key].reverseAnimation != undefined ? this.options[key].reverseAnimation.duration || 0 : 1.5,
+                            easing: this.options.reverseAnimation != undefined ? this.options.movement[key].reverseAnimation.easing || 'linear' : 'linear',
+                            elasticity: this.options.reverseAnimation != undefined ? this.options.reverseAnimation.elasticity || null : null,
+                            scaleX: 1,
+                            scaleY: 1,
+                            scaleZ: 1,
+                            translateX: 0,
+                            translateY: 0,
+                            translateZ: 0,
+                            rotateX: 0,
+                            rotateY: 0,
+                            rotateZ: 0
+                        });                    
+                    }
+                });
+                this.DOM.el.addEventListener('mouseenter', this.mouseenterFn);
+                this.DOM.el.addEventListener('mousemove', this.mousemoveFn);
+                this.DOM.el.addEventListener('mouseleave', this.mouseleaveFn);
+            }
+            
+            tilt(ev) {
+                if ( !allowTilt ) return;
+                const mousepos = getMousePos(ev);
+                // Document scrolls.
+                const docScrolls = {
+                    left : document.body.scrollLeft + document.documentElement.scrollLeft, 
+                    top : document.body.scrollTop + document.documentElement.scrollTop
+                };
+                const bounds = this.DOM.el.getBoundingClientRect();
+                // Mouse position relative to the main element (this.DOM.el).
+                const relmousepos = { 
+                    x : mousepos.x - bounds.left - docScrolls.left, 
+                    y : mousepos.y - bounds.top - docScrolls.top 
+                };
+                
+                // Movement settings for the animatable elements.
+                for (let key in this.DOM.animatable) {
+                    if ( this.DOM.animatable[key] == undefined || this.options[key] == undefined ) {
+                        continue;
+                    }
+                    
+                    let t = this.options[key] != undefined ? this.options[key].translation || {x:0,y:0,z:0} : {x:0,y:0,z:0},
+                        r = this.options[key] != undefined ? this.options[key].rotation || {x:0,y:0,z:0} : {x:0,y:0,z:0};
+
+                    setRange(t);
+                    setRange(r);
+                    
+                    const transforms = {
+                        translation : {
+                            x: (t.x[1]-t.x[0])/bounds.width*relmousepos.x + t.x[0],
+                            y: (t.y[1]-t.y[0])/bounds.height*relmousepos.y + t.y[0],
+                            z: (t.z[1]-t.z[0])/bounds.height*relmousepos.y + t.z[0],
+                        },
+                        rotation : {
+                            x: (r.x[1]-r.x[0])/bounds.height*relmousepos.y + r.x[0],
+                            y: (r.y[1]-r.y[0])/bounds.width*relmousepos.x + r.y[0],
+                            z: (r.z[1]-r.z[0])/bounds.width*relmousepos.x + r.z[0]
+                        }
+                    };
+
+                    this.DOM.animatable[key].style.WebkitTransform = this.DOM.animatable[key].style.transform = 'translateX(' + transforms.translation.x + 'px) translateY(' + transforms.translation.y + 'px) translateZ(' + transforms.translation.z + 'px) rotateX(' + transforms.rotation.x + 'deg) rotateY(' + transforms.rotation.y + 'deg) rotateZ(' + transforms.rotation.z + 'deg)';
+                }
             }
         }
-    }
-    
-    class Grid {
-        constructor(el) {
-            this.DOM = {el: el};
-            this.items = [];
-            Array.from(this.DOM.el.querySelectorAll('a.project-item')).forEach((item) => {
-                const itemObj = new Item(item);
-                this.items.push(itemObj);
-            });
+        
+        class Grid {
+            constructor(el) {
+                this.DOM = {el: el};
+                this.items = [];
+                Array.from(this.DOM.el.querySelectorAll('a.project-item')).forEach((item) => {
+                    const itemObj = new Item(item);
+                    this.items.push(itemObj);
+                });
+            }
         }
+
+        let allowTilt = true;
+        new Grid(document.querySelector('.project-list'));
+
+
+    //both these closed brackets necessary DO NOT DELETE i know its tempting
     }
-
-    let allowTilt = true;
-    new Grid(document.querySelector('.project-list'));
 }
-
-}
-// more
+// onContentReplaced Ends here
 
 const swup = new Swup();
 
