5d7f636b37fb7b1d164b1a8a1abb1b2bf6e9d977
Siddharth <mail@siddharthjha.com>
2020-04-06 15:35:52 +0530

    implemented portfolio slider

--- js/app.js
+++ js/app.js
@@ -180,6 +180,22 @@ function burgerToggler(){
     });
 }
 
+let winsize;
+const calcWinsize = () => winsize = {width: window.innerWidth, height: window.innerHeight};
+calcWinsize();
+window.addEventListener('resize', calcWinsize);
+const MathUtils = {
+    lineEq: (y2, y1, x2, x1, currentVal) => {
+        // y = mx + b 
+        var m = (y2 - y1) / (x2 - x1), b = y1 - m * x1;
+        return m * currentVal + b;
+    },
+    lerp: (a, b, n) => (1 - n) * a + n * b,
+    getRandomFloat: (min, max) => (Math.random() * (max - min) + min).toFixed(2)
+};
+
+
+
 //---------------------------------------------------------------
 
 initPointer();
@@ -189,17 +205,22 @@ barba.init({
     transitions: [{
         leave(data) {
             const done = this.async();
-            fluidOverlay.toggle();
-            burger.style.zIndex = "4"
-            setTimeout( done, 1200);
+            gsap.to(data.current.container, {
+                opacity: 0,
+                duration: 0.3,
+                ease: Power4.easeOut,
+                onComplete: done
+            });
         },
         enter(data){
             window.scrollTo(0, 0);
-            fluidOverlay.toggle();
             onPageLoad();
-            setTimeout( () => {
-                burger.style.zIndex = "1"
-            }, 1000);
+            gsap.from(data.next.container, {
+                opacity: 0,
+                duration: 0.3,
+                ease: Power4.easeIn,
+                delay: 0.5
+            });
         }
     }, {
         name: 'home-to-project',
@@ -219,7 +240,6 @@ barba.init({
                     this.async();
                 }
             });
-
         },
         enter(data){
             onPageLoad();
@@ -228,15 +248,15 @@ barba.init({
                 duration: 0.6,
                 width: 0,
             }).from(".project-title, .project-description, .project-metadata", {
-                duration: 0.3,
+                duration: 0.2,
                 opacity: 0,
                 y: 10,
-                stagger: 0.2
+                stagger: 0.1
             }).from("article > *", {
-                duration: 0.5,
+                duration: 0.2,
                 opacity: 0,
                 y: 10,
-                stagger: 0.2
+                stagger: 0.1
             })
         }
     }]
@@ -247,16 +267,138 @@ function onPageLoad() {
     burgerToggler();
     colourHeaderByBG();
 
-    window.addEventListener('scroll', () => {
-        if (window.scrollX > 200) {
-            document.getElementById('home').classList.add('fade');
-        } else if (window.scrollX < 200){
-            document.getElementById('home').classList.remove('fade');
+    const hideHomeHeader = gsap.timeline({paused: true});
+    hideHomeHeader.to('#home', {zIndex: 0}, 0.2);
+    hideHomeHeader.to( '#home > h1', {x: -80, opacity: 0,}, "<");
+    hideHomeHeader.to( '#home > p', {x: -80, opacity: 0,}, "<");
+    hideHomeHeader.to( '.avatar', {x: -80, opacity: 0, duration: 0.3}, "<0.3");
+
+    class SliderItem {
+        constructor(el) {
+            this.DOM = {el: el};
+            this.DOM.image = this.DOM.el.querySelector('.img-inner');
+            this.DOM.title = this.DOM.el.querySelector('.project-link');
         }
-    });
+    }
+
+    class DraggableSlider {
+        constructor(el) {
+            this.DOM = {el: el};
+            this.DOM.strip = this.DOM.el.querySelector('.project-list');
+            this.items = [];
+            [...this.DOM.strip.querySelectorAll('.project-item')].forEach(item => this.items.push(new SliderItem(item)));
+            this.DOM.draggable = this.DOM.el.querySelector('.draggable'); // draggable container
+            this.draggableWidth = this.DOM.draggable.offsetWidth; // width of draggable & strip container
+            this.maxDrag = this.draggableWidth < winsize.width ? 0 : this.draggableWidth - winsize.width; // amount that we can drag
+            this.dragPosition = 0; // The current amount (in pixels) that was dragged
+            this.draggie = new Draggabilly(this.DOM.draggable, { axis: 'x' }); // Initialize the Draggabilly
+            this.init();
+            this.initEvents();
+        }
+        init() {
+            this.renderedStyles = {
+                position: {previous: 0, current: this.dragPosition},
+                scale: {previous: 1, current: 1},
+                imgScale: {previous: 1, current: 1},
+                opacity: {previous: 1, current: 1},
+            };
+
+            this.render = () => {
+                this.renderId = undefined;
+                for (const key in this.renderedStyles ) {
+                    this.renderedStyles[key].previous = MathUtils.lerp(this.renderedStyles[key].previous, this.renderedStyles[key].current, 0.1);
+                }
+                gsap.set(this.DOM.strip, {x: this.renderedStyles.position.previous});
+                for (const item of this.items) {
+                    gsap.set(item.DOM.el, {scale: this.renderedStyles.scale.previous, opacity: this.renderedStyles.opacity.previous});
+                    gsap.set(item.DOM.image, {scale: this.renderedStyles.imgScale.previous});
+                }
+                if ( !this.renderId ) {this.renderId = requestAnimationFrame(() => this.render());}
+            };
+            this.renderId = requestAnimationFrame(() => this.render());
+        }
+
+        initEvents() {
+            this.onDragStart = () => {
+                this.renderedStyles.scale.current = 0.9;
+                this.renderedStyles.imgScale.current = 1.2;
+                this.renderedStyles.opacity.current = 0.8;
+                // cursor.scale = 1.5;
+                // cursor.showArrows();
+                // cursor effects on drag
+            };
+
+            this.onDragMove = (event, pointer, moveVector) => {
+                // The possible range for the drag is draggie.position.x = [-maxDrag,0 ]
+                if ( this.draggie.position.x >= 0 ) {
+                    // the max we will be able to drag is winsize.width/2
+                    this.dragPosition = MathUtils.lineEq(0.5*winsize.width,0, winsize.width, 0, this.draggie.position.x);
+                }
+                else if ( this.draggie.position.x < -1*this.maxDrag ) {
+                    // the max we will be able to drag is winsize.width/2
+                    this.dragPosition = MathUtils.lineEq(0.5*winsize.width,0, this.maxDrag+winsize.width, this.maxDrag, this.draggie.position.x);
+                }
+                else {
+                    this.dragPosition = this.draggie.position.x;
+                }
+                this.renderedStyles.position.current = this.dragPosition;
+                // mousepos = getMousePos(event);
+
+                hideHomeHeader.progress(window.innerWidth > 800 ? this.draggie.position.x/-400 : 0);
+            };
+
+            this.onDragEnd = () => {
+                if ( this.draggie.position.x > 0 ) { // reset draggable if out of bounds.
+                    this.dragPosition = 0;
+                    this.draggie.setPosition(this.dragPosition, this.draggie.position.y);
+                }
+                else if ( this.draggie.position.x < -1*this.maxDrag ) {
+                    this.dragPosition = -1*this.maxDrag;
+                    this.draggie.setPosition(this.dragPosition, this.draggie.position.y);
+                }
+                this.renderedStyles.position.current = this.dragPosition;
+                this.renderedStyles.scale.current = 1;
+                this.renderedStyles.imgScale.current = 1;
+                this.renderedStyles.opacity.current = 1;
+                // cursor effects on dragend
+                // cursor.scale = 1;
+                // cursor.hideArrows();
+            };
+
+            this.draggie.on('pointerDown', this.onDragStart);
+            this.draggie.on('dragMove', this.onDragMove);
+            this.draggie.on('pointerUp', this.onDragEnd);
+
+            // for (const item of this.items) {
+            //     item.DOM.title.addEventListener('click', (ev) => {
+            //         ev.preventDefault();
+            //         // this triggers the content show animation
+            //         // this.showItem(item);
+            //     });
+            // }
+
+            window.addEventListener('resize', () => {
+                this.maxDrag = this.draggableWidth < winsize.width ? 0 : this.draggableWidth - winsize.width;
+                if ( Math.abs(this.dragPosition) + winsize.width > this.draggableWidth ) {
+                    const diff = Math.abs(this.dragPosition) + winsize.width - this.draggableWidth;
+                    // reset dragPosition
+                    this.dragPosition = this.dragPosition+diff;
+                    this.draggie.setPosition(this.dragPosition, this.draggie.position.y);
+                }
+            });
+        }
+    }
+
+    // The images strip
+    new DraggableSlider(document.querySelector('.portfolio'));
+
+
+
 }
 
 onPageLoad();
+
+
 
 
 
